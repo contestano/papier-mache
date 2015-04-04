995d9d49a875dd5e4ba64272db942fc51106c6e6
pdrogfer <pdro74@hotmail.com>
2015-04-04 20:15:45 +0200

    update

---
+++ Assets/Scripts/moveCloud.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveCloud : MonoBehaviour {
+	Transform tT;
+	public float speed;
+	Vector2 newPosition;
+	// Use this for initialization
+	void Start () {
+		tT = transform;
+		calculateNewPosition ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		tT.transform.Translate (Vector2.right * - speed * Time.deltaTime);
+		if (tT.transform.position.x <= -6.6) {
+			calculateNewPosition ();
+			tT.transform.position = newPosition;
+		}
+	}
+	void calculateNewPosition() {
+		newPosition = new Vector2 (6.6f, Random.Range (1f, 3f));
+		speed = Random.Range (0.5f, 1.0f);
+	}
+}
---
+++ Assets/Scripts/playerControl.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerControl : MonoBehaviour {
+	Transform tT;
+	bool isJumping;
+	float xDirection, jumpForce, jump;
+	public float speed;
+	public GameObject player;
+	// Use this for initialization
+	void Start () {
+		tT = transform;
+		speed = 3.0f;
+		isJumping = false;
+		jumpForce = 5.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		xDirection = Input.GetAxis ("Horizontal");
+		Debug.Log (isJumping);
+		tT.transform.Translate (Mathf.Abs (xDirection) * speed * Time.deltaTime, 0f, 0f);
+		if (xDirection > 0) {
+			transform.eulerAngles = new Vector2 (0, 0);
+		}
+		if (xDirection < 0){
+			transform.eulerAngles = new Vector2 (0, 180);
+		}
+		jump = Input.GetAxis ("Jump");
+		if (jump != 0 && !isJumping) {
+			Rigidbody2D playerPhysics = (Rigidbody2D)player.GetComponent<Rigidbody2D>();
+			playerPhysics.AddForce(new Vector2(xDirection, jumpForce), ForceMode2D.Impulse);
+			isJumping = true;
+		}
+	}
+	void OnCollisionEnter2D(Collision2D col) {
+		if (col.gameObject.tag == "Ground") {
+			isJumping = false;
+		}
+	}
+}
